d2035ff42322a15e8225ac5ede45e2b7bd9c19f9
maxyma-sanx <maxymasan.dev@gmail.com>
2022-12-03 20:05:31 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-console.log('123');
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
+
+let intervalID = null;
+
+refs.start.addEventListener('click', onStartButtonClick);
+refs.stop.addEventListener('click', onStopButtonClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartButtonClick() {
+  intervalID = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.start.disabled = true;
+}
+
+function onStopButtonClick() {
+  clearInterval(intervalID);
+  refs.start.disabled = false;
+}
